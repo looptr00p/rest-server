85cacb6904c065c715a510182256823c3f89fa7e
Nicolas Herrera <nicolas.herrera@xintec.cl>
2018-08-20 16:00:11 -0300

    RestServer - Usuario

--- server/config/config.js
+++ server/config/config.js
@@ -1,1 +1,22 @@
+/**
+ * PUERTO
+ */
 process.env.PORT = process.env.PORT || 3000;
+
+/**
+ * ENTORNO
+ */
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+/**
+ * BASE DE DATOS
+ */
+let urlDB;
+
+if (process.env.NODE_ENV = 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://cafe-user:qaz!!123@ds125912.mlab.com:25912/looptroop_cafe';
+}
+
+process.env.URLDB = urlDB;
