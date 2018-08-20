993e4f2f3f8b41246e68ced5f0ed1d760340c7ee
Nicolas Herrera <nicolas.herrera@xintec.cl>
2018-08-20 17:17:25 -0300

    Varialbe de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,7 +16,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://cafe-user:qaz!!123@ds125912.mlab.com:25912/looptroop_cafe';
+    //VARAILBE EN HEROKU
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
